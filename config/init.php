12d16ac424654ce3d9b573295eea29b31e5a0310
SreeragG000 <sreeragg000@gmail.com>
2025-12-15 08:24:33 +0530

    Fix formatting issues in init.php

--- config/init.php
+++ config/init.php
@@ -4,6 +4,5 @@ require_once __DIR__ . '/db.php';
 require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/room_utils.php';
 require_once __DIR__ . '/../includes/csrf.php';
-
 define('DIR', BASE_PATH);
 ?>
