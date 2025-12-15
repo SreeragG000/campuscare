dc27a692428c2d3748d92c5200520c3f3d4e34c3
SreeragG000 <sreeragg000@gmail.com>
2025-12-15 07:32:44 +0530

    Update database name in db.php configuration

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 $DB_HOST = 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com';
 $DB_USER = '4HemRxYCqKos4PM.root';
 $DB_PASS = 'NlOYRtCpfIWeE2Nk';
-$DB_NAME = 'test';
+$DB_NAME = 'college_assets';
 $DB_PORT = 4000;
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
@@ -30,6 +30,7 @@ try {
     echo 'Database connection error: ' . $e->getMessage();
     exit;
 }
+
 
 
 
